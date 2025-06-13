079b076b589dd16e2f4136f4abd90ca40de68c3d
Mohd Zain <mohammedzain4099@gmail.com>
2025-06-13 23:21:34 +0530

    Implement All Doctor page

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,20 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-      <div className=" mx-4 sm:mx-[10%] ">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/doctors" element={<Doctors />} />
-          <Route path="/doctors/:speciality" element={<Doctors />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/my-profile" element={<MyProfile />} />
-          <Route path="/my-appointments" element={<MyAppointment />} />
-          <Route path="/appointment/:docId" element={<Appointments />} />
-        </Routes>
-      </div>
+    <div className=" mx-4 sm:mx-[10%] ">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:specialityParams" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointment />} />
+        <Route path="/appointment/:docId" element={<Appointments />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,11 +1,93 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams, NavLink, useNavigate } from "react-router-dom";
+import useAppContext from "../context/AppContext";
+import Footer from "../components/Footer";
 
 function Doctors() {
+  const navigate = useNavigate();
+  const { specialityParams } = useParams();
+  const { doctors } = useAppContext();
+  const [filteredDoctor, setFilteredDoctor] = useState([]);
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const filterDoctor = () => {
+    if (specialityParams) {
+      setFilteredDoctor(
+        doctors.filter((doctor) => doctor.speciality == specialityParams)
+      );
+    } else {
+      setFilteredDoctor(doctors);
+    }
+  };
+
+  useEffect(() => {
+    filterDoctor();
+  }, [specialityParams]);
+
+  const specialityLinks = [
+    "General physician",
+    "Gynecologist",
+    "Dermatologist",
+    "Pediatricians",
+    "Neurologist",
+    "Gastroenterologist",
+  ];
+
   return (
     <div>
-      doctor
+      <p className="text-gray-600 font-base">
+        Browse through the doctors specialist.
+      </p>
+      <button
+        onClick={() => setOpenMenu((prev) => !prev)}
+        className="sm:hidden  border border-gray-300 px-4 py-1 mt-5 rounded-sm cursor-pointer text-sm text-gray-600 "
+      >
+        Filters
+      </button>
+      <div className={`flex flex-col sm:flex-row mt-5 gap-5`}>
+        <div
+          className={`${openMenu ? "flex" : "hidden"} sm:flex flex-col gap-5`}
+        >
+          {specialityLinks.map((speciality) => (
+            <NavLink
+              to={`/doctors/${speciality}`}
+              className={({ isActive }) => `
+            border border-gray-300 rounded-md min-w-[200px] px-3 py-1.5 
+            text-sm text-gray-600 text-start 
+            ${isActive ? "bg-indigo-100 text-black" : ""}
+          `}
+            >
+              {speciality}
+            </NavLink>
+          ))}
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {filteredDoctor.map((doctor) => (
+            <div
+              key={doctor._id}
+              onClick={() => navigate(`/appointment/${doctor._id}`)}
+              className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+            >
+              <img
+                src={doctor.image}
+                alt="doctor_image"
+                className="bg-blue-50"
+              />
+              <div className="p-4">
+                <div className="flex items-center gap-2 text-sm text-center text-green-500">
+                  <p className="size-2 bg-green-500 rounded-full"></p>
+                  <p>Available</p>
+                </div>
+                <p className="text-lg font-medium">{doctor.name}</p>
+                <p className="text-gray-600 text-sm">{doctor.speciality}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Doctors
+export default Doctors;
